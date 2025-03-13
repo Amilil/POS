1424bc23474950568e7e72bf058f887afbedfb3b
Amilil <amililuing86@gmail.com>
2025-03-13 09:49:12 +0700

    Practicum-2.3 Jobsheet4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,26 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\UserModel;
-use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::where('username', 'manager9')->firstOrFail();
-        
-        return view('user', ['data'=> $user]);
+        // Menghitung jumlah pengguna dari tabel m_user
+        $jumlahPengguna = UserModel::count();
+
+        // Mengirim data ke view
+        return view('user', ['jumlahPengguna' => $jumlahPengguna]);
     }
-        // $data = [
-        //     'level_id' => 2,
-        //     'username' => 'manager_tiga',
-        //     'nama' => 'Manager 3',
-        //     'password' => Hash::make('12345'),
-        // ];
-
-        // UserModel::create($data);
-
-        // $user = UserModel::all();
-        // return view('user', ['data' => $user]);
-    }
-
+}
