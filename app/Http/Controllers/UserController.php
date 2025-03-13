d86d08ebad26cfbc357cf995a75691c4e219b974
Amilil <amililuing86@gmail.com>
2025-03-13 10:07:54 +0700

    Practicum-2.5 Jobsheet4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,21 +4,34 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\UserModel;
-use Illuminate\Support\Facades\Hash; // Tambahkan ini
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::firstOrNew(
-            [
-                'username' => 'manager33',
-                'nama' => 'Manager Tiga Tiga',
-                'password' => Hash::make('12345'),
-                'level_id' => 2
-            ],
-        );
+        // Membuat user baru dengan data awal
+        $user = UserModel::create([
+            'username' => 'manager11',
+            'nama' => 'Manager11',
+            'password' => Hash::make('12345'),
+            'level_id' => 2,
+        ]);
+
+        // Mengubah username user
+        $user->username = 'manager12';
+
+        // Menyimpan perubahan ke database
         $user->save();
-        return view('user', ['data' => $user]);
+
+        // Mengecek apakah ada perubahan setelah penyimpanan
+        $wasChangedOverall = $user->wasChanged(); // true
+        $wasChangedUsername = $user->wasChanged('username'); // true
+        $wasChangedMultiple = $user->wasChanged(['username', 'level_id']); // true
+        $wasChangedNama = $user->wasChanged('nama'); // false
+        $wasChangedNamaUsername = $user->wasChanged(['nama', 'username']); // true
+
+        // Debugging dengan dd()
+        dd($wasChangedNamaUsername);
     }
 }
