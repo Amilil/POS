b2ca559b7a81cab567c91325c237648b225c0d59
Amilil <amililuing86@gmail.com>
2025-06-11 11:24:13 +0700

    Jobsheet 9 Creating a RESTful API Register

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -6,14 +6,26 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class UserModel extends Authenticatable
+class UserModel extends Authenticatable implements JWTSubject
 {
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return[];
+    }
+
     use HasFactory;
 
     protected $table = 'm_user'; // mendefinisikan nama tabel yang digunakan oleh model ini
     protected $primaryKey = 'user_id'; // mendefinisikan primary key dari tabel yang digunakan
-    protected $fillable = ['level_id', 'username', 'nama', 'password'];
+    protected $fillable = ['level_id', 'username', 'nama', 'password', 'image'];
 
     protected $hidden = ['password']; // jangan ditampilkan saat select
 
@@ -37,5 +49,20 @@ class UserModel extends Authenticatable
     public function getRole()
     {
         return $this->level->level_kode;
+    }
+
+    // matikan kode dibawah ini kalo udah selesai js 10
+    // public function image(): Attribute
+    // {
+    //     return Attribute::make(
+    //         get: fn($image) => asset('public/uploads/profile' . $image),
+    //     );
+    // }
+
+    public function getProfilePictureUrl()
+    {
+        return $this->image
+            ? asset($this->image)
+            : asset('adminlte/dist/img/user2-160x160.jpg');
     }
 }
